5b503e274e0e6e7d5d98e721f50018f0c751a668
FelixNeoAA <felixneoaa@gmail.com>
2018-05-16 23:22:16 +0200

    publicationDao 和 responseDao
    CRUD功能

--- src/dao/PublicationDAO.java
+++ src/dao/PublicationDAO.java
@@ -1,5 +1,67 @@
 package dao;
 
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import model.Publication;
+import model.Response;
+import model.User;
+
 public class PublicationDAO {
 
+	public void create(EntityManager em, String imagePath, String comment, Date date, User user,
+			List<Response> responses) {
+		Publication pub = new Publication();
+		em.getTransaction().begin();
+		pub.setDate(date);
+		pub.setComment(comment);
+		pub.setDate(date);
+		pub.setImagePath(imagePath);
+		pub.setUser(user);
+		pub.setResponses(responses);
+		em.persist(user);
+		for (int i = 0; i < responses.size(); i++) {
+			em.persist(responses.get(i));
+		}
+		em.persist(pub);
+		
+		em.getTransaction().commit();
+	}
+
+	public Publication get(EntityManager em, int pId) {
+		return em.find(Publication.class, pId);
+	}
+
+	public List<Publication> getAll(EntityManager em) {
+		String sql = "from Publication";
+		Query query = em.createQuery(sql);
+		List<Publication> publications = query.getResultList();
+		return publications;
+	}
+
+	public boolean delete(EntityManager em, int pId) {
+		if (get(em, pId) != null) {
+			em.getTransaction().begin();
+			em.remove(get(em, pId));
+			em.getTransaction().commit();
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public void update(EntityManager em, int pId, String imagePath, String comment, Date date, User user,
+			List<Response> responses) {
+		Publication pub = get(em, pId);
+		em.getTransaction().begin();
+		pub.setComment(comment);
+		pub.setDate(date);
+		pub.setImagePath(imagePath);
+		pub.setResponses(responses);
+		em.persist(pub);
+		em.getTransaction().commit();
+	}
 }
--- src/dao/ResponseDAO.java
+++ src/dao/ResponseDAO.java
@@ -1,5 +1,62 @@
 package dao;
 
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import model.Publication;
+import model.Response;
+import model.User;
+
 public class ResponseDAO {
 
+	public void create(EntityManager em, String comment, Date date, User user, Publication publication) {
+		Response response = new Response();
+		em.getTransaction().begin();
+		response.setComment(comment);
+		response.setDate(date);
+		response.setUser(user);
+		response.setPublication(publication);
+		em.persist(user);
+		em.persist(publication);
+		em.persist(response);
+		em.getTransaction().commit();
+	}
+
+	public Response get(EntityManager em, int rId) {
+		return em.find(Response.class, rId);
+	}
+
+	public List<Response> getAll(EntityManager em) {
+		String sql = "from Response";
+		Query query = em.createQuery(sql);
+		List<Response> responses = query.getResultList();
+		return responses;
+	}
+
+	public boolean delete(EntityManager em, int rId) {
+		if (get(em, rId) != null) {
+			em.getTransaction().begin();
+			em.remove(get(em, rId));
+			em.getTransaction().commit();
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public void update(EntityManager em, int rId, String comment, Date date, User user, Publication publication) {
+		Response response = get(em, rId);
+		em.getTransaction().begin();
+		response.setComment(comment);
+		response.setDate(date);
+		response.setPublication(publication);
+		response.setUser(user);
+		em.persist(user);
+		em.persist(publication);
+		em.persist(response);
+		em.getTransaction().commit();
+	}
 }
